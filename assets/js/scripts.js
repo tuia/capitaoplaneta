1bef7d8219b1b1dc15f5b61487c90eeee4acfe1d
nathaliazxavier <nathaliazxavier@gmail.com>
2014-09-22 11:50:45 -0300

    alerta e painel de filtros

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -30,6 +30,7 @@ $(".accordion-container input[type='radio']").change(function(){
 $("checkbox").prop("indeterminate", true); // prop is jQuery 1.6+ */
 
 
+
 /*CITAÇÃO*/
 
 	/* BOTÕES DE SENTIMENTO */
@@ -43,6 +44,7 @@ $("checkbox").prop("indeterminate", true); // prop is jQuery 1.6+ */
 		}
 		return false
 	});
+
 
 	/* DROPUP DE SETIMENTOS DA BARRA DO MEC */
 	$('.mec-menu .dropdown-menu.feeling-menu .btn').not('.remove').click( function() {
@@ -70,6 +72,7 @@ $("checkbox").prop("indeterminate", true); // prop is jQuery 1.6+ */
 			return false;
 		});
 
+
 	/*MUDANÇA DE STATUS*/
 	$('.status-bar .dropdown-menu .aberto, .status-bar .dropdown-menu .aguardando, .status-bar .dropdown-menu .fechado ').click( function() {
 		var value = $(this).text(),
@@ -89,7 +92,6 @@ $("checkbox").prop("indeterminate", true); // prop is jQuery 1.6+ */
 
 		return false;
 	});
-
 	$('.status-btn').click( function() {
 		if ( !$(this).hasClass('atendimento') ) {
 			
@@ -102,10 +104,12 @@ $("checkbox").prop("indeterminate", true); // prop is jQuery 1.6+ */
 		}
 	});
 
+
 	/*CHECKBOX*/
 	$('.quote-checkbox input').change( function() {
 		$(this).parents('.quote').toggleClass('selected');
 	});
+
 
 	/*DROPDOWN MEC*/
 	$('#mec-dropdown .dropdown-menu li a').click( function() {
@@ -127,6 +131,8 @@ $("checkbox").prop("indeterminate", true); // prop is jQuery 1.6+ */
 		}
 		return false
 	});
+
+
 	/* LIDO/NÃO-LIDO */
 	$('.leitura-btn').click( function() {
 			var v = 1;
@@ -145,3 +151,136 @@ $("checkbox").prop("indeterminate", true); // prop is jQuery 1.6+ */
 
 
 
+/* PAINEL FILTRAR */
+
+/*!
+ * classie - class helper functions
+ * from bonzo https://github.com/ded/bonzo
+ * 
+ * classie.has( elem, 'my-class' ) -> true/false
+ * classie.add( elem, 'my-new-class' )
+ * classie.remove( elem, 'my-unwanted-class' )
+ * classie.toggle( elem, 'my-class' )
+ */
+
+/*jshint browser: true, strict: true, undef: true */
+/*global define: false */
+
+( function( window ) {
+
+'use strict';
+
+// class helper functions from bonzo https://github.com/ded/bonzo
+
+function classReg( className ) {
+  return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+}
+
+// classList support for class management
+// altho to be fair, the api sucks because it won't accept multiple classes at once
+var hasClass, addClass, removeClass;
+
+if ( 'classList' in document.documentElement ) {
+  hasClass = function( elem, c ) {
+    return elem.classList.contains( c );
+  };
+  addClass = function( elem, c ) {
+    elem.classList.add( c );
+  };
+  removeClass = function( elem, c ) {
+    elem.classList.remove( c );
+  };
+}
+else {
+  hasClass = function( elem, c ) {
+    return classReg( c ).test( elem.className );
+  };
+  addClass = function( elem, c ) {
+    if ( !hasClass( elem, c ) ) {
+      elem.className = elem.className + ' ' + c;
+    }
+  };
+  removeClass = function( elem, c ) {
+    elem.className = elem.className.replace( classReg( c ), ' ' );
+  };
+}
+
+function toggleClass( elem, c ) {
+  var fn = hasClass( elem, c ) ? removeClass : addClass;
+  fn( elem, c );
+}
+
+var classie = {
+  // full names
+  hasClass: hasClass,
+  addClass: addClass,
+  removeClass: removeClass,
+  toggleClass: toggleClass,
+  // short names
+  has: hasClass,
+  add: addClass,
+  remove: removeClass,
+  toggle: toggleClass
+};
+
+// transport
+if ( typeof define === 'function' && define.amd ) {
+  // AMD
+  define( classie );
+} else {
+  // browser global
+  window.classie = classie;
+}
+
+})( window );
+
+/**
+ * main.js
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2014, Codrops
+ * http://www.codrops.com
+ */
+(function() {
+
+	var bodyEl = document.body,
+		content = document.querySelector( '.content-wrap' ),
+		openbtn = document.getElementById( 'open-filter' ),
+		closebtn = document.getElementById( 'close-filter' ),
+		isOpen = false;
+
+	function init() {
+		initEvents();
+	}
+
+	function initEvents() {
+		openbtn.addEventListener( 'click', toggleMenu );
+		if( closebtn ) {
+			closebtn.addEventListener( 'click', toggleMenu );
+		}
+
+		// close the menu element if the target it´s not the menu element or one of its descendants..
+		content.addEventListener( 'click', function(ev) {
+			var target = ev.target;
+			if( isOpen && target !== openbtn ) {
+				toggleMenu();
+			}
+		} );
+	}
+
+	function toggleMenu() {
+		if( isOpen ) {
+			classie.remove( bodyEl, 'show-menu' );
+		}
+		else {
+			classie.add( bodyEl, 'show-menu' );
+		}
+		isOpen = !isOpen;
+	}
+
+	init();
+
+})();
